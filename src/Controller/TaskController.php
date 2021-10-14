05f42b04f7cc92379cf359dada57c27ad29ecd53
RABARISON Heriniaina <rabarisonheriniainamg@gmail.com>
2021-10-14 19:02:13 +0200

    Ajout d'authentification et desgin

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/task", name="task_home")
+     * @Security("is_granted('ROLE_ADMIN')")
+     */
+    public function index(): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+}
